ee130b964bcef2b4411201aeadbd5ab1ba2adc56
Bao Tran Duc <bao.tran@pyramid-consulting.com>
2017-02-10 18:20:45 +0700

    add config for react router

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react'
+
+export const Home = () => <h1>Hello from Home!</h1>
+export const Address = () => <h1>We are located at 555 Jackson St.</h1>
--- src/index.js
+++ src/index.js
@@ -1,73 +1,8 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, Link, IndexRoute } from 'react-router'
 
-// Practice with props
-function WelcomeWithProps(props) {
-  return (
-    <div>
-      <h1>Hello, {props.inputName}.</h1>
-      <h2>It is {props.currentDate.toLocaleTimeString()}. Use props.</h2>
-      <h1>----------------------------</h1>
-    </div>
-  );
-}
+import { Home, Address}  from './app'
 
-// Practice with state
-class WelcomeWithState extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, {this.props.inputName}!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}. Use state.</h2>
-        <h1>----------------------------</h1>
-      </div>
-    );
-  }
-}
-
-// state change
-class WelcomeWithStateChange extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>State Change</h1>
-        <button onClick={this.handleClick}>
-          {this.state.isToggleOn ? 'You haven\'t liked this. Click to toggle.' : 'You like this. Click to toggle.'}
-        </button>
-        <h1>----------------------------</h1>
-      </div>
-    );
-  }
-}
-
-function RootComponent(props) {
-  return (
-    <div>
-      <WelcomeWithProps inputName = "Bao Tran" currentDate={new Date()}/>
-      <WelcomeWithState inputName="Bao Tran"/>
-      <WelcomeWithStateChange/>
-    </div>
-  );
-}
-
-render(
-  <RootComponent/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Home />
+, document.getElementById('root'))
