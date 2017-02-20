630b17c24ae88c533cfc13d1626f5a2654526455
TRANDUCBAO <BaoTran@Trans-MacBook-Pro.local>
2017-02-20 23:52:23 +0700

    add delete button to task list

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import {render} from 'react-dom'
 import {Link} from 'react-router'
-
 import { connect } from 'react-redux';
+import IconButton from 'material-ui/IconButton'
 
 class TaskList extends Component {
   render() {
     const taskList = this.props.taskList;
-    const taskListRender = taskList.map(task => <li key={task.taskID}><Link to={`/taskdetail/${task.taskID}`}>{task.name}</Link></li>)
+    const taskListRender = taskList.map(function(task) { 
+        if (task.status == true) {
+          return <li key={task.taskID}><Link to={`/taskdetail/${task.taskID}`}>{task.name}</Link></li>;
+        }
+        else {
+          return (
+            <li key={task.taskID}>
+                <Link to={`/taskdetail/${task.taskID}`}>{task.name}</Link>
+                <IconButton><span className="material-icons">delete</span></IconButton>
+            </li>
+          );
+        }
+    });
     return (
       <div>
         <ul>
